7102bef6472faeffc0dbe00112f0300c7864724c
Rajendra veerelli <84975266+rajendra5811@users.noreply.github.com>
2026-07-07 16:20:06 +0530

    Add files via upload

---
+++ 3_OOPS/6_Human/human.py
@@ -0,0 +1,21 @@
+class Human:
+    def _init_(self, n, o):
+        self.name = n
+        self.occupation = o
+    
+    def do_work(self):
+        if self.occupation ==  "tennis player":
+            print(self.name, "plays tennis")
+        elif self.occupation == "actor":
+            print(self.name, "shoots a film")
+    
+    def speaks(self):
+        print( self.name, "says how are u?")
+
+tom = Human("tom cruise", "actor")
+tom.do_work()
+tom.speaks()
+
+maria = Human("maria sharapova","tennis player")
+maria.do_work()
+maria.speaks()
